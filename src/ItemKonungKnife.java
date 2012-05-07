db5749f0d2f30caab7619a3673816df98d1485a3
Alexey Makeev <trueslider@yandex.ru>
2012-05-08 01:43:42 +0400

    Новый файл

---
+++ src/ItemKonungKnife.java
@@ -0,0 +1,15 @@
+﻿package net.minecraft.src;
+
+public class ItemKonungKnife extends Item
+{
+	public ItemKonungKnife(int i)
+	{
+		super(i);
+		maxStackSize = 1;		
+	}
+	
+    public boolean isFull3D()
+    {
+        return true;
+    }
+}
