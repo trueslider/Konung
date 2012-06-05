e6ea802b6aad4e15eb01a75caff0590843ea328c
Alex Makeev <trueslider@yandex.ru>
2012-06-05 14:11:55 +0300

    Перезаливаю файлы с новой кодировкой

--- src/TileEntityKonungWindow.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class TileEntityKonungWindow extends TileEntity
-{
-        public TileEntityKonungWindow()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
