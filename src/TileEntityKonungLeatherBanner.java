2daa1f94e2f8df01d859df78cad632541cdafeb2
Evgeniy <aries063@gmail.com>
2012-06-08 19:40:21 +0400

    no work now

--- src/TileEntityKonungLeatherBanner.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.src;
-public class TileEntityKonungLeatherBanner extends TileEntity
-{
-        public TileEntityKonungLeatherBanner()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
--- src/TileEntityKonungWoodBench.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.src;
-public class TileEntityKonungWoodBench extends TileEntity
-{
-        public TileEntityKonungWoodBench()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
