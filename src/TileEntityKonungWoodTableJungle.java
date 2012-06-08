e79543ab55d6c8122e77d2e0e16cb6177af7f5f7
Evgeniy <aries063@gmail.com>
2012-06-08 19:15:36 +0400

    old

--- src/TileEntityKonungWoodTableJungle.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class TileEntityKonungWoodTableJungle extends TileEntity
-{
-        public TileEntityKonungWoodTableJungle()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
