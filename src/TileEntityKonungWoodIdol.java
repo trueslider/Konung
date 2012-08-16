ef81e61e49d57808dd8f6783eb49f40dccd5ff0b
Alex Makeev <trueslider@yandex.ru>
2012-08-16 16:52:55 +0400

    Файл переименован

--- src/TileEntityKonungWoodIdol.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class TileEntityKonungWoodIdol extends TileEntity
-{
-        public TileEntityKonungWoodIdol()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
