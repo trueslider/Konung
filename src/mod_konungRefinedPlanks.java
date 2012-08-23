2da305d38e1732f5df2dd0275b2eea86a6843bcc
Alex Makeev <trueslider@yandex.ru>
2012-08-23 16:48:45 +0400

    WIP

--- src/mod_konungRefinedPlanks.java
+++ src/mod_konungRefinedPlanks.java
@@ -2,38 +2,34 @@ package net.minecraft.src;
 
 public class mod_konungRefinedPlanks extends BaseMod
 {
-	public static final Block defPlanks = (new KonungBlockPlanks(174,0)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("defPlanks");
-	//public static final Block pinePlanks = (new KonungBlockPlanks(175,0)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("pinePlanks");
-	//public static final Block birchPlanks = (new KonungBlockPlanks(176,0)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("birchPlanks");
-	//public static final Block junglePlanks = (new KonungBlockPlanks(177,0)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("junglePlanks");
+	
+	public static final Block refinedPlanks = (new KonungBlockRefinedPlanks(179)).setHardness(2.0F).setResistance(5.0F).setStepSound(Block.soundWoodFootstep).setBlockName("refinedPlanks");
 
+	public static int oakRefinedPlanksTexture;
+	public static int spruceRefinedPlanksTexture;
+	public static int birchRefinedPlanksTexture;
+	public static int jungleRefinedPlanksTexture;
+	
 	public mod_konungRefinedPlanks()
 	{
-		ModLoader.registerBlock(defPlanks);
-		//ModLoader.registerBlock(pinePlanks);
-		//ModLoader.registerBlock(birchPlanks);
-		//ModLoader.registerBlock(junglePlanks);
+		ModLoader.registerBlock(refinedPlanks);
+				
+		oakRefinedPlanksTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/Planks_oak.png");
+		spruceRefinedPlanksTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/Planks_spruce.png");
+		birchRefinedPlanksTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/Planks_birch.png");
+		jungleRefinedPlanksTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/Planks_jungle.png");
 		
-		ModLoader.addName(defPlanks, "Def Planks");
-		//ModLoader.addName(pinePlanks, "Pine Planks");
-		//ModLoader.addName(birchPlanks, "Birch Planks");
-		//ModLoader.addName(junglePlanks, "Jungle Planks");
+    	//ModLoader.addName(new ItemStack(refinedPlanks,0), "Refined Oak Planks");
+    	//ModLoader.addName(new ItemStack(refinedPlanks,1), "Refined Spruce Planks");
+    	//ModLoader.addName(new ItemStack(refinedPlanks,2), "Refined Birch Planks");
+    	//ModLoader.addName(new ItemStack(refinedPlanks,3), "Refined Jungle Planks");
 		
-		defPlanks.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/defPlanks.png");
-		//pinePlanks.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/pinePlanks.png");
-		//birchPlanks.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/birchPlanks.png");
-		//junglePlanks.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/textures/konung/junglePlanks.png");
-		
-		//ModLoader.addRecipe(new ItemStack(defPlanks, 1), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.oakRefinedWood,1)});
-		//ModLoader.addRecipe(new ItemStack(pinePlanks, 1), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1)});
-		//ModLoader.addRecipe(new ItemStack(birchPlanks, 1), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1)});
-		//ModLoader.addRecipe(new ItemStack(junglePlanks, 1), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1)});
+		ModLoader.addRecipe(new ItemStack(refinedPlanks, 4, 0), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.refinedWood, 1, 0)});
+		ModLoader.addRecipe(new ItemStack(refinedPlanks, 4, 1), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.refinedWood, 1, 1)});
+		ModLoader.addRecipe(new ItemStack(refinedPlanks, 4, 2), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.refinedWood, 1, 2)});
+		ModLoader.addRecipe(new ItemStack(refinedPlanks, 4, 3), new Object[]{ "X",Character.valueOf('X'), new ItemStack(mod_konungRefinedWood.refinedWood, 1, 3)});
 
-	   	//create planks from vertical clear wood
- 	   	//ModLoader.addRecipe(new ItemStack(defPlanks, 1), new Object[]{ "#",  Character.valueOf('#'), new ItemStack(mod_konungRefinedWood.oakRefinedWood,1,0)});
-	   	//ModLoader.addRecipe(new ItemStack(pinePlanks, 1), new Object[]{ "#",  Character.valueOf('#'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1,1)});
-	   	//ModLoader.addRecipe(new ItemStack(birchPlanks, 1), new Object[]{ "#",  Character.valueOf('#'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1,2)});
-	   	//ModLoader.addRecipe(new ItemStack(junglePlanks, 1), new Object[]{ "#",  Character.valueOf('#'), new ItemStack(mod_konungRefinedWood.defRefinedWood,1,3)});
+		Item.itemsList[refinedPlanks.blockID] = (new ItemTree(refinedPlanks.blockID - 256, refinedPlanks)).setItemName("refinedPlanks");
 	}
 	
 
