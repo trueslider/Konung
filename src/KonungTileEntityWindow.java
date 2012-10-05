ee8d726a4a544819a779a5b9295b85774c6d850a
Alex Makeev <trueslider@yandex.ru>
2012-10-05 16:34:41 +0400

    Комментарий

--- src/KonungTileEntityWindow.java
+++ src/KonungTileEntityWindow.java
@@ -4,6 +4,6 @@ public class KonungTileEntityWindow extends TileEntity
 {
         public KonungTileEntityWindow()
         {
-         //В этом файле ничего указывать не надо, но этот файл требуется
+
         }
 }
