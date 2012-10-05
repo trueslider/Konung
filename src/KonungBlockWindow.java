59ed8e0314f914db84b24dcc784ad8bd912fea6b
Alex Makeev <trueslider@yandex.ru>
2012-10-05 16:33:48 +0400

    Оптимизация

--- src/KonungBlockWindow.java
+++ src/KonungBlockWindow.java
@@ -4,53 +4,55 @@ import java.util.Random;
 
 public class KonungBlockWindow extends BlockContainer
 {
-        private Class BenchEntityClass;
-        public KonungBlockWindow(int i, Class class1)
+
+        public KonungBlockWindow(int par1, int par2, Class class1)
         {
-                super(i, Material.wood);
-                BenchEntityClass = class1;
+        	super(par1, par2, Material.wood);
+        	this.setCreativeTab(CreativeTabs.tabBlock);
+        	this.setRequiresSelfNotify();
         }
-        //РЈРєР°Р·С‹РІР°РµС‚ TileEntity РґР»СЏ Р±Р»РѕРєР°
+
         public TileEntity getBlockEntity()
         {
-                try
-                {
-                        return (TileEntity)BenchEntityClass.newInstance();
-                }
-                catch (Exception exception)
-                {
-                        throw new RuntimeException(exception);
-                }
+        	return new KonungTileEntityWindow();
         }
-        //Р§С‚Рѕ РґСЂРѕРїР°РµС‚СЃСЏ
+
         public int idDropped(int i, Random random, int j)
         {
-                return mod_konungFurniture.itemWindow.shiftedIndex;
+                return mod_konungDecorations.itemWindow.shiftedIndex;
         }
-        //РљРѕР»РёС‡РµСЃС‚РІРѕ РґСЂРѕРїР°
+
         public int quantityDropped(Random random)
         {
                 return 1;
         }
-        //РўРёРї СЂРµРЅРґРµСЂР° РґР»СЏ Р±Р»РѕРєР°
+        
+        /*
+         * Устанавливаем тип рендера.
+         */
         public int getRenderType()
         {
-                return -1;
+                return mod_konungDecorations.modelID;
         }
-        //False Р·РЅР°С‡РёС‚ С‡С‚Рѕ Р±Р»РѕРє РЅРµ РїСЂРѕР·СЂР°С‡РЅС‹Р№
+
         public boolean isOpaqueCube()
         {
                 return false;
         }
-        //False Р·РЅР°С‡РёС‚ С‡С‚Рѕ Р±Р»РѕРє РЅРµ РѕР±С‹С‡РЅС‹Р№ РїСЂРёРјРµСЂ С‚Р°Р±Р»РёС‡РєРё, СЃС‚СѓРїРµРЅСЊРєРё Рё С‚.Рґ
+
         public boolean renderAsNormalBlock()
         {
                 return false;
         }
         
+        protected int damageDropped(int par1)
+        {
+        	return par1;
+        }
+        
        public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving)
         {
-                int p = MathHelper.floor_double((double)((par5EntityLiving.rotationYaw * 4F) / 360F) + 0.5D) & 3; //РїРѕРІРѕСЂРѕС‚ РІ СЃС‚РѕСЂРѕРЅСѓ РёРіСЂРѕРєР°
+                int p = MathHelper.floor_double((double)((par5EntityLiving.rotationYaw * 4F) / 360F) + 0.5D) & 3;
                 byte byte0 = 3;
                 
                 
@@ -71,8 +73,9 @@ public class KonungBlockWindow extends BlockContainer
                                                 byte0 = 1;
                                 }
                                 par1World.setBlockMetadataWithNotify(par2, par3, par4, byte0);
+                                
         }
-	@Override
+
 	public TileEntity createNewTileEntity(World var1) {
 
 		return new KonungTileEntityWindow();
