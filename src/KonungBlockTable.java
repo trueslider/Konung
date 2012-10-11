b467d9fe6f8d1f0d041aeb02db49e36e7da40cb5
Alex Makeev <trueslider@yandex.ru>
2012-10-11 19:47:19 +0400

    Новый файл

---
+++ src/KonungBlockTable.java
@@ -0,0 +1,86 @@
+package net.minecraft.src;
+
+import java.util.Random;
+import java.util.List;
+
+public class KonungBlockTable extends BlockContainer
+{
+
+        public KonungBlockTable(int par1, int par2, Class class1)
+        {
+        	super(par1, par2, Material.wood);
+        	this.setCreativeTab(CreativeTabs.tabBlock);
+        	this.setRequiresSelfNotify();
+        }
+
+        public TileEntity getBlockEntity()
+        {
+        	return new KonungTileEntityTable();
+        }
+        
+        public int idDropped(int i, Random random, int j)
+        {
+                return mod_konungDecorations.itemTable.shiftedIndex;
+        }
+
+        public int quantityDropped(Random random)
+        {
+                return 1;
+        }
+        
+        /*
+         * Устанавливаем тип рендера.
+         */
+        public int getRenderType()
+        {
+                return mod_konungDecorations.modelTableID;
+        }
+
+        public boolean isOpaqueCube()
+        {
+                return false;
+        }
+
+        public boolean renderAsNormalBlock()
+        {
+                return false;
+        }
+        
+        protected int damageDropped(int par1)
+        {
+        	return par1;
+        }
+        
+       public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving)
+        {
+                int p = MathHelper.floor_double((double)((par5EntityLiving.rotationYaw * 4F) / 360F) + 0.5D) & 3;
+                byte byte0 = 3;
+                
+                
+                                if (p == 0)
+                                {
+                                                byte0 = 4;
+                                }
+                                if (p == 1)
+                                {
+                                                byte0 = 3;
+                                }
+                                if (p == 2)
+                                {
+                                                byte0 = 2;
+                                }
+                                if (p == 3)
+                                {
+                                                byte0 = 1;
+                                }
+                                par1World.setBlockMetadataWithNotify(par2, par3, par4, byte0);
+                                
+        }
+
+	public TileEntity createNewTileEntity(World var1) 
+	{
+
+		return new KonungTileEntityTable();
+	}
+       
+}
---
+++ src/KonungTileEntityTable.java
@@ -0,0 +1,9 @@
+package net.minecraft.src;
+
+public class KonungTileEntityTable extends TileEntity
+{
+        public KonungTileEntityTable()
+        {
+        	
+        }
+}
