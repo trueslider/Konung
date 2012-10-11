440a5b1c198832d6e022147d9cbee387351cb005
Alex Makeev <trueslider@yandex.ru>
2012-10-11 19:46:37 +0400

    Переименовал переменные окна, добавил стол

--- src/mod_konungDecorations.java
+++ src/mod_konungDecorations.java
@@ -6,22 +6,26 @@ public class mod_konungDecorations extends BaseMod
 	 * Устанавливаем ID блока и предмета.
 	 */	
 	public static int blockWindowID = 160;
+	public static int blockTableID = 161;
 
 	/*
 	 * Переменная для хранения ссылки на модель. 
 	 */
-	public static int modelID; 
+	public static int modelWindowID; 
+	public static int modelTableID;
 	
 	/* 
 	 * Создаем блок.
 	 */
-	public static final Block blockWindow = (new KonungBlockWindow(blockWindowID, 0, KonungTileEntityWindow.class)).setHardness(2.0F).setResistance(1.0F).setStepSound(Block.soundWoodFootstep).setBlockName("window").setCreativeTab(CreativeTabs.tabBlock);
-
+	public static final Block blockWindow = (new KonungBlockWindow(blockWindowID, 0, KonungTileEntityWindow.class)).setHardness(2.0F).setResistance(1.0F).setStepSound(Block.soundWoodFootstep).setBlockName("Window").setCreativeTab(CreativeTabs.tabBlock);
+	public static final Block blockTable = (new KonungBlockTable(blockTableID, 0, KonungTileEntityTable.class)).setHardness(2.0F).setResistance(1.0F).setStepSound(Block.soundWoodFootstep).setBlockName("Table").setCreativeTab(CreativeTabs.tabBlock);
+	
 	/*
 	 * blockWindowID-256 связывает ID блока с предметом.
 	 */
-	public static final Item itemWindow = (new KonungItemWindow (blockWindowID-256, blockWindow).setItemName("window")).setTabToDisplayOn(CreativeTabs.tabBlock);
-
+	public static final Item itemWindow = (new KonungItemWindow (blockWindowID-256, blockWindow).setItemName("Window")).setTabToDisplayOn(CreativeTabs.tabBlock);
+	public static final Item itemTable = (new KonungItemTable (blockTableID-256, blockTable).setItemName("Table")).setTabToDisplayOn(CreativeTabs.tabBlock);
+	
 
 	
 	public mod_konungDecorations()
@@ -30,23 +34,31 @@ public class mod_konungDecorations extends BaseMod
 		/*
 		 * Устанавливаем имя блока.
 		 */
-		ModLoader.addName(new ItemStack(blockWindow, 1, 0), "Window");
+		ModLoader.addName(new ItemStack(blockWindow, 1, 0), "Oak Window");
+		ModLoader.addName(new ItemStack(blockWindow, 1, 0), "ru_RU", "Дубовое окно");
+		ModLoader.addName(new ItemStack(blockTable, 1, 0), "Oak Table");
+		ModLoader.addName(new ItemStack(blockTable, 1, 0), "ru_RU", "Дубовый стол");
+
 		
 		/*
 		 * Регистрируем TileEntity.
 		 */
 		ModLoader.registerTileEntity(KonungTileEntityWindow.class, "WindowTileEntity", new KonungRenderWindow());
+		ModLoader.registerTileEntity(KonungTileEntityTable.class, "TableTileEntity", new KonungRenderTable());
 		
 		
 		/*
 		 * Добавляем рецепт блока.
 		 */
-		ModLoader.addRecipe(new ItemStack(blockWindow, 1, 0), new Object[] { "#X#", "XYX", "#X#", Character.valueOf('X'), Block.planks ,Character.valueOf('Y'), Block.thinGlass});
+		ModLoader.addRecipe(new ItemStack(blockWindow, 1, 0), new Object[] { " X ", "XYX", " X ", Character.valueOf('X'), new ItemStack (mod_konungRefinedPlanks.refinedPlanks, 1, 0) ,Character.valueOf('Y'), Block.thinGlass});
+		ModLoader.addRecipe(new ItemStack(blockTable, 1, 0), new Object[] { "XXX", "X X", "X X", Character.valueOf('X'), new ItemStack(mod_konungRefinedPlanks.refinedPlanks, 1, 0)});
+		
 
 		/*
 		 * Задаем уникальный ID для блока модели.
 		 */
-		modelID = ModLoader.getUniqueBlockModelID(this, true);
+		modelWindowID = ModLoader.getUniqueBlockModelID(this, true);
+		modelTableID = ModLoader.getUniqueBlockModelID(this, true);
 
 	}
 		
@@ -60,6 +72,10 @@ public class mod_konungDecorations extends BaseMod
 		if (var2 == blockWindow)
 		{
 			TileEntityRenderer.instance.renderTileEntityAt(new KonungTileEntityWindow(), 0.0D, 0.0D, 0.0D, 0.0F);
+		}
+		if (var2 == blockTable)
+		{
+			TileEntityRenderer.instance.renderTileEntityAt(new KonungTileEntityTable(), 0.0D, 0.0D, 0.0D, 0.0F);
 		}
 	}	
 	
