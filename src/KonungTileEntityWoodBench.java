b40b9c83dd1e3fa463449ccb71f6f55fe4f7dc11
Alex Makeev <trueslider@yandex.ru>
2012-10-12 19:39:54 +0400

    Файл переименован

--- src/KonungTileEntityWoodBench.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class KonungTileEntityWoodBench extends TileEntity
-{
-        public KonungTileEntityWoodBench()
-        {
-         //В этом файле ничего указывать не надо, но этот файл требуется
-        }
-}
