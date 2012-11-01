bf369334f5d81514af8c468eb79f296e25bb2e65
Alex Makeev <trueslider@yandex.ru>
2012-11-01 12:06:22 +0300

    Удалено

--- src/KonungBlockHay.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class KonungBlockHay extends Block
-{
-    public KonungBlockHay(int par1, int par2)
-    {
-        super(par1, par2, Material.grass);
-    }
-}
