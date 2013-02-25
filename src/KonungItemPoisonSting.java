b1ff019f078007bef027cfba61696c75e305c731
Alex Makeev <trueslider@yandex.ru>
2013-02-25 17:04:24 +0300

    fix

--- src/KonungItemPoisonSting.java
+++ src/KonungItemPoisonSting.java
@@ -1,12 +1,10 @@
 package net.minecraft.src;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
-
 public class KonungItemPoisonSting extends Item
 {
 	public KonungItemPoisonSting(int i)
 	{
 		super(i);
-		maxStackSize = 64;
+		setMaxStackSize(64);
 	}
 }
